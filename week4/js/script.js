e988e554d2d5ef3420e46a3afb25c00d732f99ee
Ryan <tian.shic@northeastern.edu>
2025-01-30 12:34:02 -0800

    Write a function that find students that live in Vancouver

--- week4/js/script.js
+++ week4/js/script.js
@@ -3,3 +3,35 @@ do {
     user = prompt("What is your name?");
 } while (isNaN(user) === false || user.length === 1);
 // console.log(user);
+
+let students = [
+{
+	name: "Cristian",
+	age: 30,
+	location: "Vancouver"
+ },
+ {
+	name: "James",
+	age: 40,
+	location: "Toronto"
+ },
+ {
+	name: "Garry",
+	age: 20,
+	location: "Vancouver"
+ }
+];
+
+
+function findStudentsInVancouver () 
+{
+    for (let i = 0; i < students.length; i++)
+    {
+        if (students[i].location === "Vancouver")
+        {
+            console.log(students[i].name);
+        }
+    }
+}
+
+findStudentsInVancouver();
