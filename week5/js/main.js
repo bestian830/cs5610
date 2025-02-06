ed830d2516aba688f3dfa954f980fb0702dbf79c
Ryan <tian.shic@northeastern.edu>
2025-02-06 11:23:15 -0800

    activity 1

---
+++ week5/js/main.js
@@ -0,0 +1,21 @@
+const radius = document.querySelector("#radius");
+const resultP = document.querySelector("#result");
+
+function getNumber() {
+    let radius = prompt("Enter a number for a circle radius");
+    return radius;
+}
+
+function calculateArea() {
+    const number = getNumber();
+    console.log(number);
+    // let's check if the number variable is numeric or not
+    if (isNaN(number)) {
+        return "Error: You entered invalid number";
+    }
+    else {
+        return (Math.PI * Math.pow(number, 2)).toFixed(2);
+    }
+}
+
+console.log(calculateArea());
