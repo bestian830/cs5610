7f865f7ee366e2c9d2340241840e5e15f2b0d6ae
Ryan <tian.shic@northeastern.edu>
2025-02-13 11:52:59 -0800

    require exports functionality

--- week6/app.js
+++ week6/app.js
@@ -1,18 +1,22 @@
-// use writefile to write a text to a file
-const fs = require('fs');
+// // use writefile to write a text to a file
+// const fs = require('fs');
 
 // fs.writeFile("data.txt", "This is a message for you!", (err) => {if(err) {
 //     console.log("write failed");
 // } else {
 //     console.log("write successful");
+//     // read the message back and log it to the console.
+//     fs.readFile("data.txt", "utf8", (err, data) => {
+//         if(err) {
+//             console.log("read failed");
+//         } else {
+//             console.log(data);
+//         }
+//     });
 // }
 // });
 
-// read the message back and log it to the console.
-fs.readFile("data.txt", "utf8", (err, data) => {
-    if(err) {
-        console.log("read failed");
-    } else {
-        console.log(data);
-    }
-});
+
+const logger = require("./logger.js");
+// logger.log(logger);
+logger.log();
