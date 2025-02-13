aa0e15823b7b2748b7b9f5e776417d51c5ac2f4d
Ryan <tian.shic@northeastern.edu>
2025-02-13 11:38:53 -0800

    read the message successful

--- week6/app.js
+++ week6/app.js
@@ -1,9 +1,17 @@
 // use writefile to write a text to a file
 const fs = require('fs');
 
-fs.writeFile("data.txt", "This is a message for you!", (err) => {if(err) {
-    console.log("write failed");
-} else {
-    console.log("write successful");
-}
+// fs.writeFile("data.txt", "This is a message for you!", (err) => {if(err) {
+//     console.log("write failed");
+// } else {
+//     console.log("write successful");
+// }
+// });
+
+fs.readFile("data.txt", "utf8", (err) => {
+    if(err) {
+        console.log("read failed");
+    } else {
+        console.log("read successful");
+    }
 });
