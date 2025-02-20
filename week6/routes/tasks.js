182c3f4e71211b50d5d36d150dedfa0109588e8c
Ryan <tian.shic@northeastern.edu>
2025-02-20 12:44:40 -0800

    last changes

--- week6/routes/tasks.js
+++ week6/routes/tasks.js
@@ -3,25 +3,46 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+router.get("/", async (req,res)=> {
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/todos/");
+        // console.log(response.data);
+        res.json(response.data);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+    
+})
+
 
 // this is the handler for /tasks
-router.get("/", (req,res)=> {
-    // send a get request to jsonplaceholder api
-    const promise = axios.get("https://jsonplaceholder.typicode.com/todos/");
-    console.log(promise);
-    promise.then((response) => {
+// router.get("/", (req,res)=> {
+//     // send a get request to jsonplaceholder api
+//     const promise = axios.get("https://jsonplaceholder.typicode.com/todo/");
+//     console.log(promise);
+//     promise.then((response) => {
+//         res.json(response.data);
+//     }).catch((error) => {
+//         res.status(500).send(error.message);
+//     });
+// })
+
+router.get("/:taskId", async (req,res)=> {
+    try {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${req.params.taskId}`);
         res.json(response.data);
-    }).catch((error) => {
-        res.status(500).send(error.message);
-    });
+        // console.log(promise);
+        // console.log(req.params.taskId);
+        // res.send(`<p>You are viewing task ${req.params.taskId}</p>`);
+        res.render("task", {
+            id : req.params.taskId,
+            title : response.data.title,
+            completed : response.data.completed});
+    }
+    catch (err) {
+        res.status(500).send(err.message);
+    }
 })
 
-router.get("/:taskId", (req,res)=> {
-    console.log(req.params.taskId);
-    // res.send(`<p>You are viewing task ${req.params.taskId}</p>`);
-    res.render("task", {id : req.params.taskId});
-})
 
-
-
-module.exports = router;
+module.exports = router;    
