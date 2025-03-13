04a2015ca823a235fb1ee574a8a38db7a6cff8d2
Ryan <tian.shic@northeastern.edu>
2025-03-13 11:46:52 -0700

    - Create a file named Header.jsx and write a functional component in it. Move the <h1> out of App.jsx and have the new Header component return the <h1> inside a <header> tag.     - Good practice: save all your components in a folder called "components" in the src folder. - Import the new Header component in App.jsx and have App.jsx return <Header /> - How can we pass the application name to the component as a variable?     - Hint: [props](https://react.dev/learn/passing-props-to-a-component): All JSX attributes and children of a user-defined component are passed to it ass a single object.

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
 
 export default function App() {
   const appName = "My Awesome App";
   return (
     <div>
-      <h1>
+      {/* <h1>
         Welcome to {appName}
-      </h1>
+      </h1> */}
+      <Header myAppName={appName} />
     </div>
   )
 }
