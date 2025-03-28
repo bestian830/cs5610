fca6ce4697ac7d028fc9086a93bbaa191ae479fe
Ryan <tian.shic@northeastern.edu>
2025-03-27 23:26:25 -0700

    modify all

--- week6_practice/logger.js
+++
@@ -1,9 +0,0 @@
-let version = 2.6;
-function log()
-{
-    console.log('logged successfully');
-}
-module.exports = {
-    log,
-    version
-};
--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -1,33 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import Header from './components/Header'
-import TasksList from './components/TasksList'
-import AddTask from './components/AddTask';
+import React, { use, useEffect, useState } from "react";
+import Header from "./components/Header";
+import AddTask from "./components/AddTask";
+import TasksList from "./components/TasksList";
+import { NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function App() {
-  const [_tasks, _setTasks] = useState([]);
-  async function fetchData() {
-    try {
-      const response = await fetch("http://localhost:5001/todos");
+  const appName = "My React App";
+  const [tasksFromServer, setTasksFromServer] = useState([]);
+  const [showAddTask, setShowAddTask] = useState(false);
+
+
+  async function fetchDate() {
+    try{
+      // const response = await fetch("http://localhost:5001/tasks", { signle });
+      const response = await fetch("http://localhost:5001/tasks");
       if (response.ok) {
         const data = await response.json();
-        _setTasks(data);
+        setTasksFromServer(data); 
       } else {
-        throw new Error("fetch failed with status code")
+        throw new Error("fetch failed");
       }
-    } catch (err) {
-      console.log("fetchData", err);
+    } catch (error) {
+      console.log("fetchData", error);
     }
-  }
-  useEffect(() => {
-    fetchData();
+    
+} 
+  useEffect(() => { 
+    fetchDate();
   }, []);
 
-  const appName = "My Awesome App";
+  function toggleShowAddTask() {
+    setShowAddTask((prev) => !prev);
+  }
+
   return (
-    <div className='appContainer'>
-      <Header myAppName={appName} />
-      <AddTask />
-      <TasksList tasks={_tasks}/>
+    <div className="appContainer">
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/tasks">Tasks</NavLink>
+      </nav>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header 
+                myAppName={appName} 
+                version={2}
+                onToggleAddTask={toggleShowAddTask}
+                showAddTask={showAddTask}
+              />
+              {showAddTask && <AddTask />}
+            </>
+          }
+        />
+        <Route path="/tasks" element={<TasksList tasks={tasksFromServer} />} />
+        <Route path="*" element={<h1>This page doesn't exist.</h1>} />
+      </Routes>
+
     </div>
   );
 }
