53a65c6beb34eff1439c389b315ec2974d0160d0
Ryan <tian.shic@northeastern.edu>
2025-04-03 17:22:03 -0700

    Create LogoutButton function component

---
+++ week11_activity11/client/src/components/LogoutButton.jsx
@@ -0,0 +1,24 @@
+// src/components/LogoutButton.jsx
+
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button 
+      onClick={() => logout({
+        logoutParams: {
+          // 登出后重定向到应用首页
+          returnTo: window.location.origin
+        }
+      })}
+      className="btn btn-danger"
+    >
+      登出
+    </button>
+  );
+};
+
+export default LogoutButton;
