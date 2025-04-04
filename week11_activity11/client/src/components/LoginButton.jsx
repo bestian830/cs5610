84d451b4e21e4aaa16d175b925229a5872fe7f39
Ryan <tian.shic@northeastern.edu>
2025-04-03 17:12:44 -0700

    Create login button component

---
+++ week11_activity11/client/src/components/LoginButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+    // 从Auth0上下文中获取loginWithRedirect方法
+    const { loginWithRedirect } = useAuth0();
+
+    return (
+        <button
+            onClick={() => loginWithRedirect()}
+            className="btn btn-primary"
+        >
+            Log in
+        </button>
+    );
+};
+
+export default LoginButton;
