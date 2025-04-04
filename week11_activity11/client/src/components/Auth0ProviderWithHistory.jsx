a2b57b10137b2c4e13a33e78c0555ea6a9b64e82
Ryan <tian.shic@northeastern.edu>
2025-04-03 18:24:48 -0700

    modify auth0providerwithhistory component

--- week11_activity11/client/src/components/Auth0ProviderWithHistory.jsx
+++ week11_activity11/client/src/components/Auth0ProviderWithHistory.jsx
@@ -1,35 +1,35 @@
-import { Auth0Provider } from '@auth0/auth0-react';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-    // useNavigate是React Router提供的钩子,用于编程试导航
-    const navigate = useNavigate();
+  // useNavigate是React Router提供的钩子,用于编程试导航
+  const navigate = useNavigate();
 
-    // 从环境变量中获取Auth0配置信息
-    const domain = import.meta.env.VITE_AUTO_DOMAIN;
-    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  // 从环境变量中获取Auth0配置信息
+  const domain = import.meta.env.VITE_AUTO_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
-    // 当Auth0重定向回应用时的回调函数
-    const onRedirectCallback = (appState) => {
-        // 导航到用户之前尝试访问的页面,如果没有则导航到首页
-        navigate(appState?.returnTo || window.location.pathname);
-    };
+  // 当Auth0重定向回应用时的回调函数
+  const onRedirectCallback = (appState) => {
+    // 导航到用户之前尝试访问的页面,如果没有则导航到首页
+    navigate(appState?.returnTo || window.location.pathname);
+  };
 
-    // Auth0Provider组件包装整个应用,提供认证上下文
-    return (
-        <Auth0Provider
-            domain={domain}
-            clientId={clientId}
-            authorizationParams={{
-                // 重定向URL(登陆成功后返回的地址)
-                redirect_uri: window.location.origin,
-            }}
-            onRedirectCallback={onRedirectCallback}
-            >
-                {children}
-            </Auth0Provider>
-    );
+  // Auth0Provider组件包装整个应用,提供认证上下文
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        // 重定向URL(登陆成功后返回的地址)
+        redirect_uri: window.location.origin,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 export default Auth0ProviderWithHistory;
